88b7752bc7201abe8f3b2fead2798d34e6fb90a7
Pawan Bharti <bpawan96@gmail.com>
2025-01-10 02:37:30 +0530

    Error handling

--- app/controllers/calculators_controller.rb
+++ app/controllers/calculators_controller.rb
@@ -1,11 +1,14 @@
 class CalculatorsController < ApplicationController
   def add
+    raise "No string passed." if params[:string].nil?
     digits = params[:string].scan(/-?\d+/).map(&:to_i).reject { |num| num > 1000 }
     
     if (negative_values = digits.select(&:negative?)).any?
-      render json: { error: "negatives not allowed #{negative_values}" }
+      raise "negatives not allowed #{negative_values}."
     else
       render json: { output: digits.sum }
     end
+  rescue => e
+    render json: { error: e.message }
   end
 end
--- spec/controllers/calculators_spec.rb
+++ spec/controllers/calculators_spec.rb
@@ -4,27 +4,32 @@ RSpec.describe CalculatorsController, type: :controller do
   describe "add" do
     context 'with valid input' do
       it 'returns the sum of numbers' do
-        post :add, params: { string: '1,2,3' }
+        get :add, params: { string: '1,2,3' }
         expect(JSON.parse(response.body)).to eq('output' => 6)
       end
 
       it 'ignores numbers greater than 1000' do
-        post :add, params: { string: '2,1001,3,9999' }
+        get :add, params: { string: '2,1001,3,9999' }
         expect(JSON.parse(response.body)).to eq('output' => 5)
       end
     end
 
     context 'with negative numbers' do
       it 'raises an error when negative numbers are present' do
-        post :add, params: { string: '-1,2,-3' }
-        expect(JSON.parse(response.body)).to eq('error' => 'negatives not allowed [-1, -3]')
+        get :add, params: { string: '-1,2,-3' }
+        expect(JSON.parse(response.body)).to eq('error' => 'negatives not allowed [-1, -3].')
       end
     end
 
     context 'with invalid input' do
       it 'handles special characters' do
-        post :add, params: { string: '///1***2\n3;4%5[6]' }
+        get :add, params: { string: '///1***2\n3;4%5[6]' }
         expect(JSON.parse(response.body)).to eq('output' => 21)
+      end
+
+      it 'handles nil value' do
+        get :add
+        expect(JSON.parse(response.body)).to eq('error' => 'No string passed.')
       end
     end
   end
